e3353131461a473c44892a521a84085771e3958e
hasanchobarawala <hasanchobarawala@gmail.com>
2025-09-15 16:30:39 +0530

    Update page.tsx

--- src/app/src/app/tree/page.tsx
+++ src/app/src/app/tree/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import StartButton from "@/components/StartButton";
 import { useEffect, useState } from "react";
 import { onAuthStateChanged, User } from "firebase/auth";
 import { auth } from "@/lib/firebase";
